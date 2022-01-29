f5e8db50ce0075742eba74972c15ca80019aee3b
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-01-29 16:03:13 +0000

    fixes #52

--- 0_Season_Fire_Determinate.js
+++ 0_Season_Fire_Determinate.js
@@ -47,4 +47,6 @@ Map.add(legend);
 //________BASEMAP_____________________
 var mapStyle = [{elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},{elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
   {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
-  {featureType: 'administrative',elementType: 'geome
+  {featureType: 'administrative',elementType: 'geometry.stroke', stylers: [{color: '#c9b2a6'}]},
+  {featureType: 'administrative.land_parcel',elementType: 'geometry.stroke',stylers: [{color: '#dcd2be'}]},
+  {featureType
