1f3b690edfbfacf21f870c4b702baedd3f522848
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-02-14 06:03:37 +0000

    more shit

--- 0_Season_Fire_Determinate.js
+++ 0_Season_Fire_Determinate.js
@@ -51,4 +51,5 @@ var mapStyle = [{elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},{elemen
   {featureType: 'administrative.land_parcel',elementType: 'geometry.stroke',stylers: [{color: '#dcd2be'}]},
   {featureType: 'administrative.land_parcel',elementType: 'labels.text.fill',stylers: [{color: '#ae9e90'}] },
   {featureType: 'administrative.land_parcel',elementType: 'labels.text.stroke',stylers: [{color: '#000040'}, {visibility: 'simplified'}]  },
-  {featureType: 'a
+  {featureType: 'administrative.neighborhood', elementType: 'labels.text.fill',stylers: [{color: '#408080'}]},
+  {featureType: 'landsca
