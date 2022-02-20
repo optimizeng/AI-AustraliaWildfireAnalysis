289f3389ee4e99586dcdcbf210f4893cda564da2
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-02-20 11:12:25 +0000

    Modified   ManaPHP/Application.php

--- 0_Season_Fire_Determinate.js
+++ 0_Season_Fire_Determinate.js
@@ -52,4 +52,5 @@ var mapStyle = [{elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},{elemen
   {featureType: 'administrative.land_parcel',elementType: 'labels.text.fill',stylers: [{color: '#ae9e90'}] },
   {featureType: 'administrative.land_parcel',elementType: 'labels.text.stroke',stylers: [{color: '#000040'}, {visibility: 'simplified'}]  },
   {featureType: 'administrative.neighborhood', elementType: 'labels.text.fill',stylers: [{color: '#408080'}]},
-  {featureType: 'landsca
+  {featureType: 'landscape.man_made',elementType: 'geometry.fill',stylers: [{color: '#800040'}]},
+  {featureType: 'landscape.natural',elementType: 'geometry'
