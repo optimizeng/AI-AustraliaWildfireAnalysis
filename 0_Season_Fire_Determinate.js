f20cb0ad89535fc276bd8a0a565719690097d55e
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-03-06 22:42:36 +0000

    hamradio/yam: fix info leak in ioctl

--- 0_Season_Fire_Determinate.js
+++ 0_Season_Fire_Determinate.js
@@ -63,4 +63,5 @@ var mapStyle = [{elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},{elemen
   {featureType: 'poi.park',elementType: 'geometry.fill',stylers: [{color: '#a5b076'}]},
   {featureType: 'poi.park',elementType: 'labels.text.fill',stylers: [{color: '#447530'}]},
   {featureType: 'road',elementType: 'geometry',stylers: [{color: '#f5f1e6'}]},
-  {featureType: 'road',
+  {featureType: 'road',elementType: 'labels.icon',stylers: [{visibility: 'off'}]},
+  {featureType: 'road.arterial',elementType: 'geometry',stylers: [{color:
