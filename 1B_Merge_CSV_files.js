4b4a01c9dcd3d046f25a2f687485d8ff11b9e3c3
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-04-10 17:41:27 +0000

    Add a screenshot.

--- 1B_Merge_CSV_files.js
+++ 1B_Merge_CSV_files.js
@@ -2,4 +2,9 @@ var training = table.merge(table2).merge(table3).merge(table4).merge(table5).mer
             .merge(table10).merge(table11).merge(table12).merge(table13).merge(table14).merge(table15).merge(table16).merge(table17).merge(table18)
            
 Export.table.toDrive({
-  collection: t
+  collection: training,
+  description: 'test',
+  fileFormat: 'CSV'});              
+
+var active = training.filterMetadata("fire","equals",1)
+var no
