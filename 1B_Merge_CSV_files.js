4311045ae4588bb6d9d32e3d45d240519d97a575
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-04-10 16:44:36 +0000

    bump 0.1.0

--- 1B_Merge_CSV_files.js
+++ 1B_Merge_CSV_files.js
@@ -1,2 +1,5 @@
 var training = table.merge(table2).merge(table3).merge(table4).merge(table5).merge(table6).merge(table7).merge(table8).merge(table9)
-            .merge(table10).merge(table11).merge(table12).merge(table13)
+            .merge(table10).merge(table11).merge(table12).merge(table13).merge(table14).merge(table15).merge(table16).merge(table17).merge(table18)
+           
+Export.table.toDrive({
+  collection: t
