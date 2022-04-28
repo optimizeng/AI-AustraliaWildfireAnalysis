1f751d9d40e45def18a93bfb07f61233550c9dcd
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-04-28 14:31:28 +0000

    Fix typos in README.

--- 2C_CART_ML.js
+++ 2C_CART_ML.js
@@ -8,4 +8,5 @@ var Australia_shp_geometry = Australia_shp.geometry();
 
 
 //______Import States in Australia____________________________________________
-var Capi
+var Capital_AUS = Australia_shp.filterMetadata("name","equals","Australian Capital Territory");
+var Northen_AUS = Australia_shp.filterMetadata("cc"
