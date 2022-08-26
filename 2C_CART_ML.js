fdd2bd5fbd4b0d8f4697fe02401a9d588bb02ff0
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-08-26 06:20:55 +0000

    vis

--- 2C_CART_ML.js
+++ 2C_CART_ML.js
@@ -114,4 +114,5 @@ Map.addLayer(GHM_index, {min:0, max:1, palette:palette_GHM}, 'Global Human Modif
 // 8 MODIS NDVI 250m
 var dataset = ee.ImageCollection('MODIS/006/MOD13Q1').filter(ee.Filter.date('2019-08-01', '2019-09-01'));
 var ndvi = dataset.select('NDVI').mean().clip(Australia);;
-var ndviVis = { min: 0.0, max: 8000.0
+var ndviVis = { min: 0.0, max: 8000.0,palette: ['FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901',
+    '66A000', '529400', '3E8601', '207401', '056201', '004C00', '
