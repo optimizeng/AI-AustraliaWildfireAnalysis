70685b55f9d0923e84cf20982cae7ccdd589432a
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-09-14 17:50:47 +0000

    removed console.log()

--- 2C_CART_ML.js
+++ 2C_CART_ML.js
@@ -142,4 +142,7 @@ Map.addLayer(temp_max, vsVis, 'Maximum temperature  Scale 0,1',0);
 // 13 Palmer Drought Severity Index https://developers.google.com/earth-engine/datasets/catalog/IDAHO_EPSCOR_TERRACLIMATE#description
 var Drought_Palmer= ee.ImageCollection('IDAHO_EPSCOR/TERRACLIMATE').filter(ee.Filter.date('2018-08-31', '2019-08-31'));
 var Drought_Palmer = Drought_Palmer.select('pdsi').reduce(ee.Reducer.mean()).clip(Australia);
-var DroughVis = { min:-300,max: 100,palette: ['40C778','6D855D','C0BEA
+var DroughVis = { min:-300,max: 100,palette: ['40C778','6D855D','C0BEA0','CD947B','E5E6E4'],};
+Map.addLayer(Drought_Palmer, DroughVis, 'Palmer Drought Severity Index', 0);
+
+// 14 Precipitation accumul
