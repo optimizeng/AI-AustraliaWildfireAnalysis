2c6d46fdafffb3aa70f558f0a1f479278ea59996
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-12-04 18:08:26 +0000

    Update .bowerrc

--- 3B_EA_Fire_FIRMS_Daily.js
+++ 3B_EA_Fire_FIRMS_Daily.js
@@ -25,4 +25,5 @@ Map.addLayer(FIRMS_AUS_Total, visTp,'Total fire Jan-Feb');
 var legend = ui.Panel({style: { position: 'middle-right',padding: '8px 15px'}});
 var legendTitle = ui.Label({value: '', style: {fontWeight: 'bold',fontSize: '18px',margin: '0 0 4px 0',padding: '0'}});
 legend.add(legendTitle);
-var makeRow = function(color, name) {var color
+var makeRow = function(color, name) {var colorBox = ui.Label({style: {backgroundColor: '#' + color, padding: '8px',margin: '0 0 4px 0'}});
+     var description = ui.Label({value:name,style: {margi
