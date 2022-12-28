bb148d0b7f47a5b8b95fb84776c445a6a4fa2456
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-12-28 07:10:53 +0000

    drivers/scsi/sg.c: mark VMA as VM_IO to prevent migration

--- 3B_EA_Fire_FIRMS_Daily.js
+++ 3B_EA_Fire_FIRMS_Daily.js
@@ -63,4 +63,5 @@ var mapStyle = [{elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},{elemen
   {featureType: 'transit.line', elementType: 'geometry',stylers: [{color: '#dfd2ae'}]},
   {featureType: 'transit.line',elementType: 'labels.text.fill',stylers: [{color: '#8f7d77'}]},
   {featureType: 'transit.line',elementType: 'labels.text.stroke',stylers: [{color: '#ebe3cd'}]},
-  
+  {featureType: 'transit.station',elementType: 'geometry',stylers: [{color: '#dfd2ae'}]},
+  {featureType: 'water',elementType: 'geometr
