496df48007062ca08caa88873f94085291b40d6a
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-12-28 19:39:46 +0000

    about page update

--- 3C_EA_Precipitation_CHIRPS_Monthly.js
+++ 3C_EA_Precipitation_CHIRPS_Monthly.js
@@ -1,1 +1,7 @@
 var Australia = ee.FeatureCollection("USDOS/LSIB/2013").filterMetadata("cc","equals","AS")
+Map.addLayer(Australia);
+Map.centerObject(Australia, 4);
+
+// Daily total precipitation sums
+var CHIRPS = ee.ImageCollection('UCSB-CHG/CHIRPS/DAILY')
+    .f
