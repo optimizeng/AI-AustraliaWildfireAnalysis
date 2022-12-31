06c0b5e735cca8a05ee5caa5eed59c79fab02ca3
optimizeng <124293815+optimizeng@users.noreply.github.com>
2022-12-31 08:34:04 +0000

    v1.8.2

--- 3C_EA_Precipitation_CHIRPS_Monthly.js
+++ 3C_EA_Precipitation_CHIRPS_Monthly.js
@@ -5,4 +5,8 @@ Map.centerObject(Australia, 4);
 // Daily total precipitation sums
 var CHIRPS = ee.ImageCollection('UCSB-CHG/CHIRPS/DAILY')
     .filter(ee.Filter.date('2019-09-01','2020-02-29'));
-var CHIRPS_AUS = CHIRPS.map(function(Landsat) { return Landsat.clip
+var CHIRPS_AUS = CHIRPS.map(function(Landsat) { return Landsat.clip(Australia); });
+
+var monchart=ui.Chart.image.series(CHIRPS_AUS, Australia, ee.Reducer.mean())
+  .setOptions({
+    title: "Daily Pe
