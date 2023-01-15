068f63bcc87c89590b0f5d4f92cedea49069dc3e
optimizeng <124293815+optimizeng@users.noreply.github.com>
2023-01-15 13:47:14 +0000

    qr code

--- 3C_EA_Precipitation_CHIRPS_Monthly.js
+++ 3C_EA_Precipitation_CHIRPS_Monthly.js
@@ -55,4 +55,5 @@ var mapStyle = [
   {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
   {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
   {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
-  {featureTy
+  {featureType: 'administrative',elementType: 'geometry.stroke',stylers: [{color: '#c9b2a6'}] },
+  {featureType: 'administrative.land_parcel',e
