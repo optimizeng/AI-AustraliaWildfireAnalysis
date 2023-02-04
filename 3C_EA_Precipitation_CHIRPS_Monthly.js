ab4f37ca248f279ab78e8dea9b2cdbc5f1dc20ff
optimizeng <124293815+optimizeng@users.noreply.github.com>
2023-02-04 16:55:50 +0000

    sync: add tracepoint support

--- 3C_EA_Precipitation_CHIRPS_Monthly.js
+++ 3C_EA_Precipitation_CHIRPS_Monthly.js
@@ -68,4 +68,5 @@ var mapStyle = [
   {featureType: 'road.highway',elementType: 'geometry',stylers: [{color: '#f8c967'}]},
   {featureType: 'road.highway',elementType: 'geometry.stroke',stylers: [{color: '#e9bc62'}]},
   {featureType: 'road.local',elementType: 'labels.text.fill',stylers: [{color: '#806b63'}]},
-  {featureType: 'water'
+  {featureType: 'water', elementType: 'geometry.fill',stylers: [{color: '#b9d3c2'}] }, 
+  {featureType: 'water',elementType: 'labels.text.fill',stylers: [{color: 'blue'}]}];
