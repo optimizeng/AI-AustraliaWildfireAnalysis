458b05fec21af99ea599b45b9d411d1b2c460520
optimizeng <124293815+optimizeng@users.noreply.github.com>
2023-02-05 15:12:33 +0000

    Updated core.

--- 3D_EA_SoilMoisture_Monthly.js
+++ 3D_EA_SoilMoisture_Monthly.js
@@ -1,1 +1,6 @@
-var Australia = ee.FeatureCollection("USDOS/LSIB/2013").filterMetadata(
+var Australia = ee.FeatureCollection("USDOS/LSIB/2013").filterMetadata("cc","equals","AS")
+Map.addLayer(Australia);
+Map.centerObject(Australia, 4);
+
+var startyear = 2018; //Your start year
+var endyear = 2020; //Your end ye
